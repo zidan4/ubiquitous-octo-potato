3c8b6563732e613a2e3f1c6733ba45b21bcd2969
zidan4 <zidanjaffar@gmail.com>
2026-09-01 22:24:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def greetings( name="You" ):
+  return f"hallo {name}"
+
+print(greetings("alex"))
+
+def info( *args, **kwargs ):
+  print(f"args: {args}")
+  print(f"kwargs: {kwargs}")
+
+info(1, 2, 3, name="alex", age=30)
+
+
+courses = [ "python", "java", "javascript" ]
+students = { "alex": 30, "bob": 24, "charlie": 28 }
+
+def info2( *args, **kwargs ):
+  print( args )
+  print( kwargs )
+
+info2( *courses, **students )
